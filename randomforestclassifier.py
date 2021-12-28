bd74f4ed9b3e363fa85b9016da972f6214edb260
nmani31 <85704262+nmani31@users.noreply.github.com>
2021-12-28 17:39:17 -0500

    Add files via upload

---
+++ randomforestclassifier.py
@@ -0,0 +1,21 @@
+import pandas as pd
+dataset = pd.read_csv('/Users/sridharmani/Desktop/brown lab/datasetfinal-1.csv')
+from sklearn.model_selection import train_test_split
+independent = dataset[['VOP','NOP','POP']]
+dependent = dataset['motility']
+dependent_train, dependent_test, independent_train, independent_test = train_test_split(dependent, independent, test_size=0.2)
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier(n_estimators=200)
+clf.fit(independent_train,dependent_train)
+from sklearn import metrics
+a = []
+b = []
+for i in range (10000):
+    dependent_pred = clf.predict(independent_test)
+    accuracy = metrics.accuracy_score(dependent_pred, dependent_test)
+    a.append(accuracy)
+    precision = metrics.precision_score(dependent_pred, dependent_test)
+    b.append(precision)
+    i = i + 1
+print('Accuracy', sum(a)/len(a))
+print('Precision', sum(b)/len(b))
